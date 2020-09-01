41a8dcb2415ebc0bbafdb661180849b63d5bec97
红鲤鱼与绿鲤鱼与驴 <60869237+wangdelu2020@users.noreply.github.com>
2020-09-01 10:41:14 +0800

    Update quwalk2.2.js

--- quwalk2.2.js
+++ quwalk2.2.js
@@ -1,17 +1,17 @@
 /*=================================
-邀请码 5KNCVN 关注微信公众号iosrule和微信群 
+邀请码 5KNCVN
 下载地址https://apps.apple.com/cn/app/%E8%B6%A3%E8%B5%B0%E5%B0%8A%E4%BA%AB%E7%89%88-%E8%B5%B0%E8%B7%AF%E8%B5%9A%E9%92%B1app/id1465888732
 
 功能:跑步，签到，打卡，步数兑换,偷步等.
 攻略，阅读看视频玩游戏
 作者红鲤鱼绿鲤鱼与驴  2020.8.6
 2020.8.7 修复很多bug
-2020.8.27 增加跑步
+2020.8.27 增加跑步,20200901修复字符
 2020.8.28 增加每日5000步挑战
 
 //=================================
 #圈叉趣走App签到
-https:\/\/mobile01\.91quzou\.com\/rebate\/qz\/task\/homeTaskView\.do url script-request-header quwalk2.2.js
+https:\/\/mobile01\.91quzou\.com\/rebate\/qz\/task\/homeTaskView\.do url script-request-header quwalk.js
 
 
 mit=mobile01.91quzou.com
@@ -19,7 +19,7 @@ mit=mobile01.91quzou.com
 //====================================
 
 #loon 趣走App签到
-http-request https:\/\/mobile01\.91quzou\.com\/rebate\/qz\/task\/homeTaskView\.do script-path=quwalk2.2.js, requires-body=true, timeout=30, tag=趣走签到
+http-request https:\/\/mobile01\.91quzou\.com\/rebate\/qz\/task\/homeTaskView\.do script-path=quwalk.js, requires-body=true, timeout=30, tag=趣走签到
 
 mit=mobile01.91quzou.com
 
@@ -582,6 +582,7 @@ printlog(result1+result2);
 function quwalk_ptodayWalk(ios)
 {
   var jsbox=getcoding(ios);
+
 const llUrl3={
         url:"https://mobile01.91quzou.com/walk/submitWalk.do",headers:JSON.parse(quwalk_hd),body:jsbox}
   $iosrule.post(llUrl3,function(error, response, data) {
@@ -597,7 +598,8 @@ const llUrl3={
 function getcoding(iosrule)
 {const quwalk_co="walkRecord=";
 var rule="="+iosrule;
-  var decode=new Date();var inv=decode.getMonth()+1;if(inv<10)inv="0"+inv;var it=decode.getDate();if(it<10)inv="0"+inv;var code=quwalk_co+decode.getFullYear().toString()+inv.toString()+it.toString()+rule;
+  var decode=new Date();var inv=decode.getMonth()+1;if(inv<10)inv="0"+inv;var it=decode.getDate();
+if(it<10)it="0"+it;var code=quwalk_co+decode.getFullYear().toString()+inv.toString()+it.toString()+rule;
   return code;
 }
 
